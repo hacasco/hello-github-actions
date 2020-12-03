9e1bebc7877efc0ceab2c8701eed7c291c9f930a
Hugo Casco <hugo.casco@aurea.com>
2020-12-03 09:55:26 -0300

    Este es otro commit que NO se ejecuta

--- a.ps1
+++ a.ps1
@@ -1,6 +1,6 @@
 Write-Host "Inicio"
-Start-Sleep -s 10
-Write-Host "Opa 10 segundos"
+Start-Sleep -s 5
+Write-Host "Opa 5 segundos"
 
 Write-Output "::set-output name=opa::true"
 Write-Host "este es el branch-4"
